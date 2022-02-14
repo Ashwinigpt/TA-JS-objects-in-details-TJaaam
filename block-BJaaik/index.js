784d90ddd21f17b8fb05815b47608cb04652721d
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-02-14 15:54:39 +0530

    block-BJaaik

---
+++ block-BJaaik/index.js
@@ -0,0 +1,16 @@
+class person{
+    constructor(name, age, gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    eat(){
+
+    }
+    sleep(){
+        
+    }
+    walk(){
+        
+    }
+}
